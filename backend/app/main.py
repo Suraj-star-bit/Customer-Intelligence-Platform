fcac97a5520607bdaec449d854a124abefdfc4cb
Suraj-star-bit <surajyamme19@gmail.com>
2026-08-01 13:11:21 +0530

    Fix production CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,6 @@ from app import database
 from app.routes import customer
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import analytics
-
 from app.database import Base, engine
 from app.models import Customer
 
@@ -14,9 +13,9 @@ Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
-    "https://your-vercel-url.vercel.app"
-],
+        "http://localhost:5173",
+        "https://customer-intelligence-platform-k4t34qwed.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -25,11 +24,13 @@ app.add_middleware(
 app.include_router(customer.router)
 app.include_router(analytics.router)
 
+
 @app.get("/")
 def root():
     return {
         "message": "Customer Intelligence API is running"
     }
+
 
 @app.get("/health")
 def health_check():
